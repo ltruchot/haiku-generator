318f2f0b73eccb5bcd8c73da124d557555f5945d
Loïc TRUCHOT <loic.truchot.pro@gmail.com>
2020-04-06 16:24:23 +0200

    chore(master): dist

--- wasm-demo/dist/wasm-demo.e31bb0bc.js
+++ wasm-demo/dist/wasm-demo.e31bb0bc.js
@@ -304,7 +304,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49750" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53839" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
